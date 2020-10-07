83f6a1e6b8f2c7a0326a2909464a920c2f01c6c4
GeunWoo <9nugunu@likelion.org>
2020-10-07 13:24:33 +0900

    계정생성 구현

--- login_app/views.py
+++ login_app/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from login_app.forms import UserForm
 # Create your views here.
+
+def signup(request):
+    # post일 경우 사용자 생성
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('QnA_main')
+    else:
+        form = UserForm()
+    # get일 경우 홈페이지 호출
+    return render(request, 'signup.html', {'form':form})
+
