06dc35b8f6f7598a01f37c81b68828cfd2114f3f
GeunWoo <9nugunu@likelion.org>
2020-10-18 00:02:42 +0900

    페이징, 로그인해야 글쓰기/답변 가능 기능 구현

--- QnA_app/models.py
+++ QnA_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
  #Create your models here.
 
 class QnaModel(models.Model):
-   author = models.CharField(max_length=10, null=False)
+   author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
    title = models.CharField(max_length=100, null=False)
    content = models.TextField(null=False)
-   docfile = models.FileField(default="No files", upload_to="document/upload/")
+   docfile = models.FileField(default="", upload_to="document/upload/")
    created_date = models.DateTimeField(auto_now_add=True)
    modified_date = models.DateTimeField(auto_now=True)
    hits = models.PositiveIntegerField(default = 0)
@@ -20,6 +21,7 @@ class QnaModel(models.Model):
 
 
 class Answer(models.Model):
+   author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
    question = models.ForeignKey(QnaModel, on_delete=models.CASCADE)
    content = models.TextField(null=False)
    created_date = models.DateTimeField(auto_now_add=True)
